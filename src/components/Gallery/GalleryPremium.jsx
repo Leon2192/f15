1765af48676324202e7620a92f3896b66d4c8e0d
leon2192 <leon.strokes@gmail.com>
2025-07-26 17:40:40 -0300

    fc

--- src/components/Gallery/GalleryPremium.jsx
+++ src/components/Gallery/GalleryPremium.jsx
@@ -39,7 +39,7 @@ const Gallery = () => {
   });
 
   return (
-    <Box ref={ref} sx={{ py: 8, px: 2, backgroundColor: "#faafe3", maxWidth: "1200px", mx: "auto" }}>
+    <Box ref={ref} sx={{ py: 8, px: 2, backgroundColor: "#fab2b6", maxWidth: "1200px", mx: "auto" }}>
       <Typography
         variant="h3"
         sx={{
